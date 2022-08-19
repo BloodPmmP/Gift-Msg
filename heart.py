c89794592e680afb13f2cef4b0811849de1fc0b3
BloodPmmP <101040509+BloodPmmP@users.noreply.github.com>
2022-08-19 17:15:40 +0530

    Update heart.py

--- heart.py
+++ heart.py
@@ -1,7 +1,29 @@
-import turtle
-draw = turtle.Turtle()
-win = draw.screen
-win.bgcolor('black')
+import turtle as t
+import random
+
+t.speed(20)
+t.bgcolor('black')
+t.hideturtle()
+
+for x in range(250):
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+
+    t.colormode(255)
+    t.color(r, g, b)
+    t.begin_fill()
+
+    t.up()
+
+    t.goto(random.randint(-800, 800), random.randint(-600, 600))
+    t.down()
+
+    t.write('🧡', font=('Calibri', random.randint(9, 28), 'bold'))
+    t.end_fill()
+
+draw = t.Turtle()
+
 def curve():
     draw.pen(pencolor="white", pensize=3, speed=5)
     for i in range(200):
@@ -10,10 +32,10 @@ def curve():
 
 def heart():
     draw.pen(pencolor="white",fillcolor="red", pensize=3, speed=5)
-    draw.shape('turtle')
+    draw.shape("turtle")
     draw.shapesize(1,1,1)
     draw.begin_fill()
-    draw.lt(140)
+    draw.lt(50)
     draw.fd(113)
     curve()
     draw.lt(120)
@@ -23,5 +45,243 @@ def heart():
 
     draw.hideturtle()
 
+
+window = t.Screen()
+window.bgcolor('black')
+
+draw.penup()
+draw.goto(-80,300)
+draw.pendown()
+draw.shapesize(1,2,1)
+
+draw.pen(pencolor="white",fillcolor="yellow", pensize=3, speed=1.001)
+
+draw.begin_fill()
+
+draw.fd(160)
+draw.rt(90)
+draw.fd(25)
+draw.rt(90)
+draw.fd(60)
+draw.lt(90)
+
+draw.fd(140)
+draw.lt(90)
+draw.fd(60)
+draw.rt(90)
+draw.fd(25)
+draw.rt(90)
+draw.fd(160)
+draw.rt(90)
+draw.fd(25)
+draw.rt(90)
+draw.fd(60)
+draw.lt(90)
+draw.fd(140)
+draw.left(90)
+draw.fd(60)
+draw.rt(90)
+draw.fd(25)
+
+draw.end_fill()
+
+draw.penup()
+draw.goto(-550,-20)
+draw.pendown()
+
+draw.pen(pencolor="white",fillcolor="yellow", pensize=3, speed=2)
+draw.begin_fill()
+
+draw.rt(90)
+draw.fd(25)
+draw.rt(90)
+draw.fd(165)
+draw.lt(90)
+draw.fd(115)
+draw.rt(90)
+draw.fd(25)
+draw.rt(90)
+draw.fd(140)
+draw.rt(90)
+draw.fd(190)
+draw.rt(90)
+
+draw.end_fill()
+
+draw.penup()
+draw.fd(140)
+
+draw.fd(70)
+
+draw.pen(pencolor="white",fillcolor="yellow", pensize=3, speed=8)
+draw.begin_fill()
+
+draw.rt(90)
+draw.fd(190)
+draw.lt(90)
+draw.pendown()
+draw.circle(60)
+draw.lt(90)
+draw.penup()
+draw.fd(20)
+draw.rt(90)
+draw.pendown()
+draw.circle(40)
+draw.rt(90)
+draw.penup()
+draw.fd(20)
+draw.lt(90)
+
+draw.end_fill()
+
+draw.fd(100)
+draw.pendown()
+
+draw.pen(pencolor="white",fillcolor="yellow", pensize=3, speed=8)
+draw.begin_fill()
+
+draw.lt(100)
+draw.fd(120)
+draw.rt(100)
+draw.fd(20)
+draw.rt(80)
+draw.fd(100)
+draw.lt(80)
+draw.fd(20)
+draw.lt(80)
+draw.fd(100)
+draw.rt(80)
+draw.fd(20)
+draw.rt(100)
+draw.fd(120)
+draw.rt(80)
+draw.fd(50)
+draw.lt(180)
+
+draw.end_fill()
+
+draw.penup()
+draw.fd(100)
+draw.pendown()
+
+draw.pen(pencolor="white",fillcolor="yellow", pensize=3, speed=8)
+draw.begin_fill()
+
+draw.lt(90)
+draw.fd(120)
+draw.rt(90)
+draw.fd(80)
+draw.rt(90)
+draw.fd(20)
+draw.rt(90)
+draw.fd(60)
+draw.lt(90)
+draw.fd(30)
+draw.lt(90)
+draw.fd(60)
+draw.rt(90)
+draw.fd(20)
+draw.rt(90)
+draw.fd(60)
+draw.lt(90)
+draw.fd(30)
+draw.lt(90)
+draw.fd(60)
+draw.rt(90)
+draw.fd(20)
+draw.rt(90)
+draw.fd(80)
+
+draw.end_fill()
+
+draw.penup()
+draw.rt(180)
+draw.fd(200)
+draw.pendown()
+
+
+draw.pen(pencolor="white",fillcolor="yellow", pensize=3, speed=2)
+draw.begin_fill()
+
+draw.lt(90)
+draw.fd(50)
+draw.lt(30)
+draw.fd(80)
+draw.rt(120)
+draw.fd(20)
+draw.rt(60)
+draw.fd(60)
+draw.lt(180)
+draw.rt(60)
+draw.fd(60)
+draw.rt(60)
+draw.fd(20)
+draw.rt(120)
+draw.fd(80)
+draw.lt(30)
+draw.fd(50)
+draw.rt(90)
+draw.fd(20)
+draw.rt(180)
+
+draw.end_fill()
+
+draw.penup()
+draw.fd(120)
+draw.pendown()
+
+
+draw.pen(pencolor="white",fillcolor="yellow", pensize=3, speed=8)
+draw.begin_fill()
+
+draw.circle(60)
+draw.lt(90)
+draw.penup()
+draw.fd(20)
+draw.pendown()
+draw.rt(90)
+draw.circle(40)
+draw.rt(90)
+draw.penup()
+draw.fd(20)
+draw.lt(90)
+
+draw.end_fill()
+
+draw.fd(100)
+draw.circle(60, extent=60)
+draw.pendown()
+
+draw.pen(pencolor="white",fillcolor="yellow", pensize=3, speed=8)
+draw.begin_fill()
+
+draw.lt(30)
+
+draw.fd(85)
+draw.lt(90)
+draw.fd(20)
+draw.lt(90)
+draw.fd(70)
+draw.circle(-20, extent=180)
+draw.fd(70)
+draw.lt(90)
+
+draw.fd(20)
+draw.lt(90)
+draw.fd(85)
+draw.circle(40, extent=180)
+
+draw.end_fill()
+
+draw.penup()
+
+draw.rt(180)
+draw.fd(35)
+draw.lt(90)
+draw.fd(140)
+draw.lt(90)
+draw.pendown()
+
 heart()
-turtle.done()
+
+t.done()
